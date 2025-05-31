bc509660193faed9a12c9a0de2bc5d01877ea9b3
dodo <palumbododo@gmail.com>
2025-05-31 16:01:35 +0200

    fix campaign

--- tf_injector/campaign.py
+++ tf_injector/campaign.py
@@ -14,52 +14,29 @@ class Campaign:
     def __init__(
         self,
         dataset_name : str, # () -> tf.data.Dataset
-        dataset_path : str,
         network_path : str,
         fautl_list_path : str,
         output_path : str,
         preporcessig: callable = lambda x: x,   # (tf.data.Dataset) -> tf.data.Dataset
         metrics_list: List[str] = None,
-        batch_size: int = 32,
-        save_scores: bool = False,
-        resume_from: int = 0,
-        validate_fault_list: bool = True,
-        seed : int = None
     ):
     
         self.dataset_name = dataset_name
         self.network_path = network_path
-        self.dataset_path = dataset_path
-        self.dataset = self.__load_dataset(dataset_path) # tf.data.Dataset
+        self.dataset = self.__load_dataset() # tf.data.Dataset
         self.network = self.__load_network()
 
         # load fault list
         self.fault_list = FaultList()
-        included_layers = self.fault_list.load_from_csv(fautl_list_path, resume_from)
+        included_layers = self.fault_list.load_from_csv(fautl_list_path)
         self.__validate_fault_list(included_layers)
-
 
         self.output_path = output_path
         self.preprocessig = preporcessig
         self.metrics_list = metrics_list
         self.metrics = self.__load_metrics()
 
-        self.num_classes = self.__get_number_of_classes()
-        self.batch_size = batch_size
-        self.save_scores = save_scores
-        self.validate_fault_list = validate_fault_list
-        self.seed = seed
-
-        # tranfrom output function
-        try:
-            self.transform_output = eval(self.preprocessig)
-        except Exception as e:
-            print(f"ERROR: lambda expression {e} impossible to tranfsorm")
-            print("Set to default labda expression 'lambda x: x'")
-            self.transform_output = lambda x: x
-
-
-    def __load_dataset(self, path) -> tf.data.Dataset :
+    def __load_dataset(self) -> tf.data.Dataset :
         dataset_name = self.dataset_name
 
 
@@ -93,7 +70,7 @@ class Campaign:
 
 
         # check if the function returns a tf.data.Dataset
-        dataset = loader_function(path)
+        dataset = loader_function()
         if not isinstance(dataset, tf.data.Dataset):
             raise TypeError(f"La funzione {func_name} non ha restituito un tf.data.Dataset")
         return dataset
@@ -161,15 +138,8 @@ class Campaign:
                 print(f"WARNING: Metric ({metric}) not found")
             
         return metrics
+    
 
-    def __get_number_of_classes(self) -> int:
-        """
-        Get the number of classes from the last layer of the network
-        """
-        last_layer = self.network.layers[-1]
-        last_layer_shape = last_layer.output_shape
-        num_classes = last_layer_shape[-1]
-        return num_classes
 
     def run(self):
         print("-------------------------------------------------------------")
@@ -178,46 +148,39 @@ class Campaign:
         injector = Injector(
             network = self.network,
             dataset = self.dataset,
-            faults = self.fault_list,
-            transform_output = self.transform_output,
-            seed= self.seed
+            faluts = self.fault_list
         )
-        
-        if (self.validate_fault_list):
-            print(f"Faul list validation...")
-            injector.validate()
 
         network_name = os.path.basename(self.network_path)
-
-        header = (
-            "inj_id",
-            "target_layer",
-            "layer_weigths",
-            "bit_pos",
-            "n_injections",
-            )
-        
-        metrics_header = tuple()
-
-        for metric in self.metrics:
-            metrics_header += metric(None, None, self.num_classes).get_header()
-
-        header += metrics_header
-
         cw = CampaignWriter(
             dataset = self.dataset_name, 
             network = network_name, 
-            report_header = header,
-            file_dir = self.output_path,
-            one_line_per_input= False
+            report_header= (
+                "col1",
+                "col2",
+                "col3",
+                "col4",
+                "col5",
+                "col6",
+                "col7"
+            ),
+            file_dir = self.output_path
             )
         
         with cw :
             injector.run_campaign(
-                batch = self.batch_size,
+                batch = 32,
                 metrics = self.metrics,
                 outputter = cw,
-                save_scores = self.save_scores
+                save_scores = True,
+                metrics_on_labels = True,
             )
         print("-------------------------------------------------------------")
         return
+
+
+    def debug(self):
+        for metric in self.metrics:
+            metric.chiSono()
+        return
+    
